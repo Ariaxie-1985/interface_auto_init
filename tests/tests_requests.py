8e87ab126a452c99ffb08e3a3d6d27f5762aab7b
ariaxie <m13910404362@163.com>
2021-01-18 21:18:23 +0800

    增加tests

---
+++ tests/tests_requests.py
@@ -0,0 +1,27 @@
+import pytest
+import code
+
+from api_script.api_script_requests_test import data_api, html_api, position_api
+
+
+def test_hello_world():
+    r = data_api()
+    if r['hello'] == 'world':
+        assert True
+
+
+def test_html():
+    r = html_api()
+    if code == 200:
+        assert True
+
+
+# def test_position_type_json():
+#     r = pisition_json_api()
+#     if r['state'] == 1:
+#         assert True
+
+def test_position_json():
+    r = position_api()
+    if r['state'] == 1:
+        assert True
